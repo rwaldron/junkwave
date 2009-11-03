0fe54d72589d73852e0a878a85f9966b93909eb1
f1lt3r <f1lt3r@f1lt3r-desktop.(none)>
2009-11-02 20:25:49 -0500

    Updating code.

--- junkwave.js
+++ junkwave.js
@@ -1,7 +1,7 @@
 /*
 
     J U N K W A V E 
-    A floating trash simulation for 5Gyres.com
+    A floating trash simulation for 5Gyres.org
     
     Developer : Alistair MacDonald, @F1LT3R, http://hyper-metrix.com
     License   : Liscenced to Brannan Novak & 5Gyres.org
@@ -18,19 +18,34 @@
   var settings = {
     
     // Path to images
-    imgPath          : "img/junkwave/",
+    imgPath           : "img/junkwave/",
 
     // Number of images available in imgPath
-    imgResorceCount  : 1,
+    imgResorceCount   : 1,
 
     // Number of pieces of junk floating around
-    junkCount        : 20,
-
-    // Number of waves
-    waveCount        : 2
+    junkCount         : 20,
+    
+    // Wave properties
+    wave              : { 
+                           count: 2,
+                            freq: { min: 0.1, max: 10  },
+                             amp: { min: 0.1, max: 1   },
+                           phase: { min: 0.1, max: 0.4 }
+                        },
+    
+    // Viscosity, LOW = Water-like, HIGH = Oil-esque
+    viscosity         : 32,
+    
+    // Radius of effect, ( how big a hand stirs the water )
+    radius            : 50,
+    
+    // Speed of animation ( milliseconds per frame )
+    speed             : 30 
     
   };
-  
+ 
+
 
   //////////////////////////////////////////////////////////////////////////////
   // JUNKWAVE OBJECT
@@ -52,24 +67,111 @@
     this.centerY = h / 2;
     
     // Build the junkwave
+    this.junk = [];
     this.addJunk();
-  }
+    this.wave = [];
+    this.setWave();
     
-  // Adds junk
-  Junkwave.prototype.addJunk = function addJunk(){
-    for( var i = 0; i < settings.junkCount; i++ ){
-      var junkImg = document.createElement( 'img' );
-      junkImg.src = settings.imgPath + 'junk_x64_01.gif';
+    
+    // Frame step animation
+    var backlink  = this;
+    var runframe  = function(){ backlink.frame() };
+    var interval  = setInterval( runframe, settings.speed );
+    
+    // Mouse move attachment
+    bindMousemove.call( this, this.elem, this.mousemove );
+  }
 
-      // Set styles for the junk pieces
-      junkImg.style.position = 'absolute';
-      junkImg.style.left = Math.random() * ( this.width - 64 ) + 'px';
-      junkImg.style.top = Math.random() * ( this.width - 64 ) + 'px';
 
-      this.elem.appendChild( junkImg );
+
+  // F R A M E - D O // ********************************************************
+  Junkwave.prototype.frame = function frame(){
+  
+    // Step through junk array
+    var l = this.junk.length;
+    for( var i = 0; i < l; i++ ){
+
+      // Alias current junk for conveinence
+      var junk = this.junk[ i ];
+
+      // Simplify for positions for readability
+      var x1 = this.elem.mouseX;
+      var y1 = this.elem.mouseY;
+      var x2 = junk.style.originX;
+      var y2 = junk.style.originX;
+      
+      // Get distance between junk and mouse
+      var distance = dist( x1, y1, x2, y2 );      
+      
+      // Junk tries to return to it's origin but is offset by human interaction, ah the patterns of life! ;)
+      junk.realX += ( junk.originX - junk.realX ) / settings.viscosity;
+      junk.realY += ( junk.originY - junk.realY ) / settings.viscosity; 
+            
+      // Map co-ords to css properties
+      junk.style.left = junk.realX + 'px';
+      junk.style.top = junk.realY + 'px';
+
+    }
+  
+  }
+  
+  // M O U S E - M O V E // ****************************************************
+  Junkwave.prototype.mousemove = function mousemove( e ){  
+    var l = this.junk.length;
+    for( var i = 0; i < l; i++ ){
+          var junk = this.junk[ i ];
+      junk.realX = this.elem.mouseX;
     }
   }
 
+  // S E T - W A V E // ********************************************************
+  Junkwave.prototype.setWave = function setWave(){
+  
+  }
+    
+  // A D D - J U N K // ********************************************************
+  Junkwave.prototype.addJunk = function addJunk(){
+    
+    for( var i = 0; i < settings.junkCount; i++ ){
+      
+      // Make a new img element for the junk
+      var junkImg = document.createElement( 'img' );
+      
+      // Decide which image is used for this piece of junk
+      junkImg.src = settings.imgPath + 'junk_x64_01.gif';
+
+      /* Set styles for the junk pieces:
+            OriginX/Y       = the location the junk tries to return to
+            realX/Y             = the current displaced x/y position
+            style.left/top  = the css properties that map realX/Y */
+      
+      junkImg.style.position = 'absolute';
+      junkImg.originX = Math.random() * ( this.width - 64 );
+      junkImg.originY = Math.random() * ( this.width - 64 );
+      junkImg.realX = junkImg.originX;
+      junkImg.realY = junkImg.originY;
+      junkImg.style.left = junkImg.realX + 'px';
+      junkImg.style.top = junkImg.realY + 'px';
+      
+      // Add new junk peice to div
+      this.elem.appendChild( junkImg );
+      
+      // Add new junk peice to array
+      this.junk[ this.junk.length ] = junkImg;
+    }
+  }
+
+
+
+  //////////////////////////////////////////////////////////////////////////////
+  // COMMON FUNCTIONS
+  //////////////////////////////////////////////////////////////////////////////
+
+  // Gets one objects distance from another
+  var dist = function dist( x1, y1, x2, y2 ){
+    return Math.sqrt( Math.pow( x2 - x1, 2 ) + Math.pow( y2 - y1, 2 ) );
+  }
+  
 
 
   //////////////////////////////////////////////////////////////////////////////
@@ -80,9 +182,8 @@
   $( document ).ready( function(){  init_junkwave() } );
 
   // Global vars...
-  var mouseX = 0,
-      mouseY = 0;
-  
+  var instances = [];
+    
 
   // Main init function that builds the waves
   function init_junkwave(){
@@ -98,15 +199,11 @@
         if( classes[ i ] == 'global' ){
           type = 'global';
         }
-      }
-            
-      return new Junkwave( this, type, w, h );
+      }        
+      
+      return instances[ instances.length ] = new Junkwave( this, type, w, h );
 
     } );
-
-
-    var debug = document.getElementById( 'debug' );    
-    bindEvents();    
     
   };
   
@@ -114,38 +211,24 @@
 
   //////////////////////////////////////////////////////////////////////////////
   // EVENTS
-  //////////////////////////////////////////////////////////////////////////////
+  //////////////////////////////////////////////////////////////////////////////    
     
-  var mouseX = 0,
-      mouseY = 0;
-    
-  var bindEvents = function(){    
-    
-    // Bind document mouseX, mouseY
-    if( !$.browser.msie ){
-      document.captureEvents( Event.MOUSEMOVE );
-    }
-
-    document.onmousemove = mouseMoved;
-    
+  var bindMousemove = function( elem, callback ){
+    junkwaveInstance = this;
+    $( elem ).mousemove( function( e ){
+        this.mouseX = e.pageX - this.offsetLeft;
+        this.mouseY = e.pageY - this.offsetTop;
+        callback ? callback.call( junkwaveInstance, { mouseX: this.mouseX, mouseY: this.mouseY } ) : 0 ;
+    } );
   }
-
-  // When the mouse was moved...
-  var mouseMoved = function mouseMoved( e ){    
-    
-    // Update the global mouse position variables
-    if( $.browser.msie ){
-      mouseX = event.clientX + document.body.scrollLeft;
-      mouseY = event.clientY + document.body.scrollTop;
+  
+  var debug = function( output ){
+    if( $.browser.mozilla ){
+      console.log( output );
     }else{
-      mouseX = e.pageX;
-      mouseY = e.pageY;
+      document.getElementById( 'debug' ).innerHTML = output;
     }
-    
-    debug.innerHTML = mouseX +','+ mouseY;
-    
   }
-
   
 
 })();
